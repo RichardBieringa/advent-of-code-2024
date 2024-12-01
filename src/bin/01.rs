7725a307685b5f050bc5a2a068e66ed6e38c914b
Richard Bieringa <RichardBieringa@gmail.com>
2024-12-01 21:37:24 +0100

    day 1 part 2

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -23,7 +25,32 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let mut frequency_map: HashMap<u32, u32> = HashMap::new();
+
+    let (left, right): (Vec<u32>, Vec<u32>) = input
+        .lines()
+        .filter_map(|line| {
+            let mut split = line.split_whitespace();
+            let left = split.next().unwrap();
+            let right = split.next().unwrap();
+
+            Some((left.parse::<u32>().ok()?, right.parse::<u32>().ok()?))
+        })
+        .unzip();
+
+    for num in right {
+        frequency_map
+            .entry(num)
+            .and_modify(|e| *e += 1)
+            .or_insert(1);
+    }
+
+    let result = left
+        .iter()
+        .map(|location| *location * frequency_map.get(location).unwrap_or(&0))
+        .sum();
+
+    Some(result)
 }
 
 #[cfg(test)]
